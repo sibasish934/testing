6d6ef3573c111ac834b2b08af6f8f45f766c74e8
Sibasish Satapathy <78403992+sibasish934@users.noreply.github.com>
2025-06-20 22:49:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// const environment = process.env.NODE_ENV || 'development';
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+// app.get('/env', (req, res) => {
+//   res.json({
+//     environment: environment,
+//     message: `You are running in ${environment} mode`
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
